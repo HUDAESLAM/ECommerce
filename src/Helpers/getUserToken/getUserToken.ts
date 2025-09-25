a9a9e10e11584c43481fb830a92b73a9536b21f3
HUDAESLAM <hudaeslam50555@gmail.com>
2025-09-25 06:43:13 +0300

    update

--- src/Helpers/getUserToken/getUserToken.ts
+++ src/Helpers/getUserToken/getUserToken.ts
@@ -3,7 +3,8 @@ import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export async function getUserToken() {
-  const token = (await cookies()).get("next-auth.session-token")?.value;
+  const token =  (await cookies()).get("next-auth.session-token")?.value ||
+    (await cookies()).get("__Secure-next-auth.session-token")?.value;
   if (!token) return null;
 
   const accessToken = await decode({
